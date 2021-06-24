a4489dd7092fb116e87ab7f24b47e4e0f841607c
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-24 09:23:38 -0700

    Service call created for pet object

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// any global variable 
+// global variables
 
+const base_url = "http://127.0.0.1:3000"
+const petService = new PetService(base_url) 
+// new instance of PetService class,
+// so when we create methods that makes the fetch request were going to need to call it on that instance methods 
 
 
 // any initializations of the application 
---
+++ src/petService.js
@@ -0,0 +1,12 @@
+// make all service calls regarding pet object 
+
+// making service call pertaining the pet object 
+
+
+class PetService{
+
+    constructor(endpoint){ 
+        this.endpoint = endpoint  // have this endpoint to reference back to rather than calling base_url everywhere 
+    }
+}
+
